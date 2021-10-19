a33a3d06421f20b906678816249ebeba01f84811
asd233 <2053044516@qq.com>
2021-10-19 22:29:35 +0800

    Team页面下的卡片组件

---
+++ src/components/card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="card">
+
+    <p>卡片类型</p>
+    <select class="
+            form-control"
+            v-model="select"
+            name=""
+            id="">
+      <option value="title">标题</option>
+      <option value="boss">BOSS</option>
+      <option value="team">阵容</option>
+      <option value="line">分割线</option>
+    </select>
+
+    <!-- <component :is='select'></component> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "card",
+  data() {
+    return {
+      select: "title"
+    }
+  }
+}
+</script>
+<style lang="less">
+#card {
+  margin: 20px 0;
+}
+</style>
